b84756872dc0d6c473bbec1f1339ff770d187ffe
jakobakoba <jakobakoba@gmail.com>
2023-01-14 16:10:35 +0600

    Revert "double type"
    This reverts commit 7a37cb89f8c770b8e2d143aed13f626f4c21d7d9.

--- src/test/kotlin/NumbersTest.kt
+++ src/test/kotlin/NumbersTest.kt
@@ -1,6 +1,5 @@
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
-import kotlin.Double.Companion.NaN
 
 internal class NumbersTest {
 
@@ -16,37 +15,6 @@ internal class NumbersTest {
         val numbers: Numbers = Numbers.Base(12, 4)
         val actual = numbers.subtract()
         val expected = 8
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun divide(){
-        val numbers: Numbers = Numbers.Base(12,4)
-        val actual = numbers.divide()
-        val expected = 3.0
-        assertEquals(expected, actual)
-    }
-    @Test
-    fun divide_double(){
-        val numbers: Numbers = Numbers.Base(10,4)
-        val actual = numbers.divide()
-        val expected = 2.5
-        assertEquals(expected, actual,0.002)
-    }
-
-    @Test
-    fun divide_zero(){
-        val numbers : Numbers = Numbers.Base(12,0)
-        val actual = numbers.divide()
-        val expected = Double.POSITIVE_INFINITY
-        assertEquals(expected,actual)
-    }
-
-    @Test
-    fun infinity_sum(){
-        val numbers : Numbers = Numbers.Doubles(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY)
-        val actual = numbers.divide()
-        val expected = NaN
         assertEquals(expected, actual)
     }
 }
